76a1015ebd7642e6e08ee978e9207455a769e82c
Bun133 <bun133.work@gmail.com>
2021-01-21 07:01:21 +0900

    修正の修正の修正

--- main/src/main/java/com/bun133/maguro/Maguro.kt
+++ main/src/main/java/com/bun133/maguro/Maguro.kt
@@ -1,7 +1,10 @@
 package com.bun133.maguro
 
 import com.destroystokyo.paper.Title
+import net.md_5.bungee.api.ChatMessageType
+import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.Bukkit
+import org.bukkit.GameMode
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -9,20 +12,27 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.event.player.PlayerRespawnEvent
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
 import org.bukkit.scoreboard.DisplaySlot
 import org.bukkit.scoreboard.Objective
 import org.bukkit.scoreboard.Scoreboard
 
-const val limitTime = 20
+//死ぬまでのtick数
+//お好みでどうぞ
+const val limitTime = 30
 var isStarting = false
 
 class Maguro : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
         this.server.pluginManager.registerEvents(MoveListener.instance, this)
-        TickCounter(MoveListener.instance, ScoreBoardUpdater(Bukkit.getScoreboardManager().newScoreboard)).runTaskTimer(this, 10, 1)
+        TickCounter(MoveListener.instance, ScoreBoardUpdater(Bukkit.getScoreboardManager().newScoreboard)).runTaskTimer(
+            this,
+            10,
+            1
+        )
         this.getCommand("maguro")!!.setExecutor(GameMaster())
     }
 
@@ -43,33 +53,45 @@ class MoveListener : Listener {
 
     @EventHandler
     fun onMove(e: PlayerMoveEvent) {
+        if (e.from.x != e.to.x || e.from.y != e.to.y || e.from.z != e.to.z){
+            map[e.player] = 0
+        }
+    }
+
+    @EventHandler
+    fun onRespawn(e:PlayerRespawnEvent){
         map[e.player] = 0
     }
 }
 
-class TickCounter(val move: MoveListener,val scoreBoardUpdater: ScoreBoardUpdater) : BukkitRunnable() {
+class TickCounter(val move: MoveListener, val scoreBoardUpdater: ScoreBoardUpdater) : BukkitRunnable() {
     override fun run() {
-        if(isStarting){
+        if (isStarting) {
             move.map.forEach { (p, i) ->
-                if (i > limitTime) {
-                    p.damage(Double.MAX_VALUE)
-                }else scoreBoardUpdater.update(p,i)
+                if (p.gameMode !== GameMode.SPECTATOR && p.gameMode !== GameMode.CREATIVE) {
+                    move.map[p] = i + 1
+                    if (i > limitTime) {
+                        p.health = 0.0
+                        p.damage(10000.0)
+                        move.map[p] = 0
+                    }
+                    scoreBoardUpdater.update(p, i)
+                }
             }
         }
     }
 }
 
-class ScoreBoardUpdater(val scoreboard: Scoreboard){
+class ScoreBoardUpdater(val scoreboard: Scoreboard) {
     val OBJECTIVE_NAME = "deathtime"
-    fun update(p:Player,i:Int){
-        var o : Objective? = scoreboard.getObjective(OBJECTIVE_NAME)
-        if(o==null){
-            o = scoreboard.registerNewObjective(OBJECTIVE_NAME, "time","0")
+    fun update(p: Player, i: Int) {
+        var o: Objective? = scoreboard.getObjective(OBJECTIVE_NAME)
+        if (o == null) {
+            o = scoreboard.registerNewObjective(OBJECTIVE_NAME, "time", "0")
             o.displaySlot = DisplaySlot.BELOW_NAME
         }
         o.getScore(p.displayName).score = i
-
-        p.sendTitle(Title("Time:${i}"))
+        p.sendActionBar("Time:${i}/${limitTime}")
     }
 }
 
@@ -83,16 +105,18 @@ class GameMaster : CommandExecutor {
     }
 
     fun onRun(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if(args.size == 1){
-            when(args[0]){
-                "s","start" -> {
+        if (args.size == 1) {
+            when (args[0]) {
+                "s", "start" -> {
                     isStarting = true
+                    return true
                 }
-                "e","end" -> {
+                "e", "end" -> {
                     isStarting = false
+                    return true
                 }
             }
-        }else return false
+        } else return false
         return false
     }
 }
